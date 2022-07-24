f0f3de656965a302292b34f382ba5ed98346bc8d
mickymacirl <mickymacster@gmail.com>
2022-07-24 18:55:55 +0000

    add js query selectors and state gamestarted, cards, dlips and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 // Javascript File
+/* Reference: https://www.webtips.dev/memory-game-in-javascript */
+/* Define Selectors */
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+};
+
+/* Reference: https://www.webtips.dev/memory-game-in-javascript */
+/* Game State */
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+};
+
