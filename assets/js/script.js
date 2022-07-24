ac7fd9349fea389d67bc61059432300f12aee62c
mickymacirl <mickymacster@gmail.com>
2022-07-24 19:11:34 +0000

    add js flipcard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,3 +103,47 @@ const flipBackCards = () => {
 
     state.flippedCards = 0;
 };
+
+/* Reference: https://www.webtips.dev/memory-game-in-javascript */
+/* flipcard function */
+const flipCard = card => {
+    state.flippedCards++;
+    state.totalFlips++;
+
+    if (!state.gameStarted) {
+        startGame();
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped');
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)');
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched');
+            flippedCards[1].classList.add('matched');
+        }
+
+        setTimeout(() => {
+            flipBackCards();
+        }, 1000);
+    }
+
+    // If there are no more cards that we can flip, we won the game
+    if (!document.querySelectorAll('.card:not(.flipped)').length) {
+        setTimeout(() => {
+            selectors.boardContainer.classList.add('flipped');
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    You won!<br />
+                    in <span class="highlight">${state.totalFlips}</span> moves.<br />
+                    with a time of <span class="highlight">${state.totalTime}</span> seconds.
+				</span>
+            `;
+
+            clearInterval(state.loop);
+        }, 1000);
+    }
+};
